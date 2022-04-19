d5956e401305be5a7dcc4e2d2f970e2de9359bcf
Мария Лисицкая <lisitskayamari@gmail.com>
2022-04-19 17:29:09 +0100

    Fixed urls

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -41,13 +41,14 @@ urlpatterns = [
         include('posts.urls')),
 ]
 
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT)
     import debug_toolbar
     urlpatterns += (
         path("__debug__/", include(debug_toolbar.urls)),)
